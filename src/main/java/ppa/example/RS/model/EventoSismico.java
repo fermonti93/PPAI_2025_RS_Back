6975f92bcdf4daf7e05d3e7c5e9fd85c4120db4c
fer <fer.monti@hotmail.com.ar>
2025-04-25 02:11:48 -0300

    cambio de tipo de fecha y admin de bs

--- src/main/java/ppa/example/RS/model/EventoSismico.java
+++ src/main/java/ppa/example/RS/model/EventoSismico.java
@@ -1,14 +1,16 @@
 package ppa.example.RS.model;
 
-import java.sql.Date;
+
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+
 import lombok.Getter;
 import lombok.Setter;
+
 
 @Entity
 @Setter
@@ -18,8 +20,8 @@ public class EventoSismico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     
-    private Data fechaHoraFin;
-    private Data fechaHoraOcurrencia;
+    private LocalDateTime fechaHoraFin;
+    private LocalDateTime fechaHoraOcurrencia;
     private double latitudEpicentro;
     private double latitudHipocetro;
     private double longitudEpicentro;
@@ -28,11 +30,11 @@ public class EventoSismico {
 
 
     //constructor del evento
-    public EventoSismico(Data ferchaHoraFin, Data ferchaHoraOcurrencia, double latitudEpicentro,
+    public EventoSismico(LocalDateTime fechaHoraFin, LocalDateTime fechaHoraOcurrencia, double latitudEpicentro,
     double latitudHipocetro, double longitudEpicentro, double longitudHipocentro,
     int valorMagnitud) {
-        this.fechaHoraFin = ferchaHoraFin;
-        this.fechaHoraOcurrencia = ferchaHoraOcurrencia;
+        this.fechaHoraFin = fechaHoraFin;
+        this.fechaHoraOcurrencia = fechaHoraOcurrencia;
         this.latitudEpicentro = latitudEpicentro;
         this.latitudHipocetro = latitudHipocetro;
         this.longitudEpicentro = longitudEpicentro;
